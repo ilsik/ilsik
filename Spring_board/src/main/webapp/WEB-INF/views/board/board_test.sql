1ba4a156a93426e714b3041a82838a90d72cf87d
ilsik <tjd2792@naver.com>
2022-01-11 19:49:06 +0900

    database추가

---
+++ Spring_board/src/main/webapp/WEB-INF/views/board/board_test.sql
@@ -0,0 +1,161 @@
+create database board_test;
+use board;
+	create table board(
+		board_no int primary key  auto_increment not null,
+		board_title varchar(20),
+		board_type varchar(20),
+		board_content varchar(1000),
+		board_name varchar(20),
+		board_pw varchar(20)
+	);
+
+insert into board values(1,"가나다","일반","위 내용은 테스트입니다1","성일식","1234");
+insert into board values(2,"가나다라","익명","위 내용은 테스트입니다2","성이식","2345");
+insert into board values(3,"가나다라마","Q&A","위 내용은 테스트입니다3","성삼식","3456");
+insert into board values(4,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(5,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(6,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(7,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(8,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(9,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(10,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(11,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(12,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(13,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(14,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(15,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(16,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(17,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board values(18,"가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+insert into board (board_title,board_type,board_content,board_name,board_pw)value("가나다라마바","자유","위 내용은 테스트입니다4","성사식","4567");
+
+select * from board where board_no='131';
+select * from board;
+select * from board where board_type='익명' order by board_no desc limit 0,10 ;
+select * from board order by board_no desc;
+create table member(
+	member_no int not null auto_increment primary key,
+	member_id varchar(20),
+    member_pw varchar(20),
+    member_name varchar(20),
+    member_gender varchar(3),
+    member_age date
+);
+drop table member;
+insert into member(member_id,member_pw,member_name,member_gender,member_age) values("admin","admin","관리자","남자","1997-01-05");
+insert into member(member_id,member_pw,member_name,member_gender,member_age) values("aaa","111","가가가","남자","1997-01-05");
+insert into member(member_id,member_pw,member_name,member_gender,member_age) values("bbb","222","나나나","여자","1997-01-05");
+insert into member(member_id,member_pw,member_name,member_gender,member_age) values("ccc","333","다다다","남자","1997-01-05");
+
+select * from member;
